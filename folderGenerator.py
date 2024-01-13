2e876fcdc9d1d0f8a4c99220a8f38d433d1ae736
Sawaiz Karim <muhammadsawaiz166@gmail.com>
2024-01-13 00:14:17 -0500

    first commit

---
+++ folderGenerator.py
@@ -0,0 +1,37 @@
+import os
+
+def getSemester():
+    return input("Please enter the semester\n")
+
+def getClasses():
+    classString = input("Enter your classes, seperated by commas (whitespace will be stripped)\n")
+    strippedString = classString.replace(" ", "")
+    return strippedString.split(',')
+
+def createSubfolders():
+   subfolderList = ["assignments", "notes", "slides-resources"] #list of subfolders to be created
+   for i in subfolderList:
+       os.mkdir(i)
+
+def createSemesterAndClass(semesterName, subjectList):
+    #creates semester folder and sets working directory to it
+    os.mkdir(semesterName)
+    os.chdir(semesterName)
+    semesterPath = os.getcwd() #used to go back to semester folder after creating one class folder
+
+    for i in subjectList: #creates class folders and subfolders
+        os.mkdir(i)
+        os.chdir(i)
+        createSubfolders()
+        os.chdir(semesterPath)
+
+def main():
+    semesterName = getSemester()
+    subjectList = getClasses()
+
+    print("Your current directory is:", os.getcwd())
+    input("Press enter to generate semester folder in this directory\n")
+    createSemesterAndClass(semesterName, subjectList)
+
+if __name__ == "__main__":
+    main()
