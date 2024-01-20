1207e563b5beae7ceb6ddeab9ef23001db877e79
Sawaiz Karim <muhammadsawaiz166@gmail.com>
2024-01-19 20:58:50 -0500

    Renamed folderGenerator to main

--- folderGenerator.py
+++
@@ -1,9 +0,0 @@
-from myFuncs import *
-import os
-
-semesterName = getSemester()
-subjectList = getClasses()
-
-print("Your current directory is:", os.getcwd())
-input("Press enter to generate semester folder in this directory\n")
-createSemesterAndClass(semesterName, subjectList)
