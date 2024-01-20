4f33b28940f51effba540bb278ff00190e77be21
Sawaiz Karim <muhammadsawaiz166@gmail.com>
2024-01-19 20:56:39 -0500

    Split functions into separate file

--- folderGenerator.py
+++ folderGenerator.py
@@ -1,38 +1,9 @@
+from myFuncs import *
 import os
 
-def getSemester():
-    semesterInput = input("Please enter the semester (whitespace will be stripped)\n")
-    return semesterInput.replace(" ", "") #didn't use .strip() because it only removed first and last whitespace
+semesterName = getSemester()
+subjectList = getClasses()
 
-def getClasses():
-    classString = input("Enter your classes, seperated by commas (whitespace will be stripped)\n")
-    strippedString = classString.replace(" ", "")
-    return strippedString.split(',') #didn't use .strip() because it only removed first and last whitespace
-
-def createSubfolders():
-   subfolderList = ["assignments", "notes", "slides-resources"] #list of subfolders to be created
-   for i in subfolderList:
-       os.mkdir(i)
-
-def createSemesterAndClass(semesterName, subjectList):
-    #creates semester folder and sets working directory to it
-    os.mkdir(semesterName)
-    os.chdir(semesterName)
-    semesterPath = os.getcwd() #used to go back to semester folder after creating one class folder
-
-    for i in subjectList: #creates class folders and subfolders
-        os.mkdir(i)
-        os.chdir(i)
-        createSubfolders()
-        os.chdir(semesterPath)
-
-def main():
-    semesterName = getSemester()
-    subjectList = getClasses()
-
-    print("Your current directory is:", os.getcwd())
-    input("Press enter to generate semester folder in this directory\n")
-    createSemesterAndClass(semesterName, subjectList)
-
-if __name__ == "__main__":
-    main()
+print("Your current directory is:", os.getcwd())
+input("Press enter to generate semester folder in this directory\n")
+createSemesterAndClass(semesterName, subjectList)
---
+++ myFuncs.py
@@ -0,0 +1,27 @@
+import os
+
+def getSemester():
+    semesterInput = input("Please enter the semester (whitespace will be stripped)\n")
+    return semesterInput.replace(" ", "") #didn't use .strip() because it only removed first and last whitespace
+
+def getClasses():
+    classString = input("Enter your classes, seperated by commas (whitespace will be stripped)\n")
+    strippedString = classString.replace(" ", "")
+    return strippedString.split(',') #didn't use .strip() because it only removed first and last whitespace
+
+def createSubfolders():
+   subfolderList = ["assignments", "notes", "slides-resources"] #list of subfolders to be created
+   for i in subfolderList:
+       os.mkdir(i)
+
+def createSemesterAndClass(semesterName, subjectList):
+    #creates semester folder and sets working directory to it
+    os.mkdir(semesterName)
+    os.chdir(semesterName)
+    semesterPath = os.getcwd() #used to go back to semester folder after creating one class folder
+
+    for i in subjectList: #creates class folders and subfolders
+        os.mkdir(i)
+        os.chdir(i)
+        createSubfolders()
+        os.chdir(semesterPath)
