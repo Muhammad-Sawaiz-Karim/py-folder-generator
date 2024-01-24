ad3e145d8fbfff0209201edea6dba263143f6cfd
karim52 <muhammadsawaiz166@gmail.com>
2024-01-24 15:06:02 -0500

    Modified to check for existing folders

--- myFuncs.py
+++ myFuncs.py
@@ -12,16 +12,19 @@ def getClasses():
 def createSubfolders():
    subfolderList = ["assignments", "notes", "slides-resources"] #list of subfolders to be created
    for i in subfolderList:
-       os.mkdir(i)
+       if (not os.path.exists(i)):
+           os.mkdir(i)
 
 def createSemesterAndClass(semesterName, subjectList):
     #creates semester folder and sets working directory to it
-    os.mkdir(semesterName)
+    if (not os.path.exists(semesterName)):
+        os.mkdir(semesterName)
     os.chdir(semesterName)
     semesterPath = os.getcwd() #used to go back to semester folder after creating one class folder
 
     for i in subjectList: #creates class folders and subfolders
-        os.mkdir(i)
+        if (not os.path.exists(i)):
+            os.mkdir(i)
         os.chdir(i)
         createSubfolders()
         os.chdir(semesterPath)
